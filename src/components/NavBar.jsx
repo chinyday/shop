3efe2bb1c18e5bb35bcb8debe8c075a5cc623989
chinyday <chinyday@gmail.com>
2023-02-09 23:11:32 +0900

    feat: 파이어베이스를 통한 로그인/아웃 작업

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { BiAddToQueue, BiCartAlt } from 'react-icons/bi'
+import {login, logout, onUserStateChange} from  '../api/firebase'
 
 export default function NavBar() {
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user)=>{
+      console.log('user', user);
+      setUser(user);
+    })
+  }, []);
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <header className="flex justify-between border-b border-gray-300 p-4">
       <Link to='/' className="flex items-center text-4xl text-brand">
@@ -12,7 +30,7 @@ export default function NavBar() {
         <Link to='/products'>products</Link>
         <Link to='/products/new' className="text-2xl"><BiAddToQueue /></Link>
         <Link to='/cart' className="text-2xl"><BiCartAlt /></Link>
-        <button>Lonin</button>
+        {user ? <button onClick={handleLogout}>Logout</button> : <button onClick={handleLogin}>Login</button>}
       </nav>
     </header>
   )

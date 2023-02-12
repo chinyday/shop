a4c532c9fd7e6f17e61627cbc9da6c400e266574
chinyday <chinyday@gmail.com>
2023-02-12 22:17:10 +0900

    feat: 상품 상세 페이지 작업 및 스타일링

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = ({product}) => {
-
-  console.log('product', product);
-
   const {title, image, price, category, id} = product;
+  const navigate = useNavigate();
+
   return (
-   <li key={id} className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+   <li key={id} className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105" onClick={()=>{navigate(`/products/${id}`, {state:{product}})}}>
     <img className='w-full' src={image} alt={title}/>
     <div className='mt-2 px-2 text-lg flex justify-between items-center'>
       <h3 className='truncate'>{title}</h3>
--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import { useLocation } from "react-router-dom";
+import Button from '../components/ui/Button'
 
 export default function ProductDetail() {
+  
+  const {state : {product : {image, title, price, description, category, options}}} = useLocation();
+  const [selectedOption, setSelectedOption] = useState(options && options[0]);
+  const handleChange = (e) => setSelectedOption(e.target.value);
+  const handleclick = (e) => {
+    // 장바구니 추가
+  }
+
   return (
-    <div>ProductDetail</div>
+   <section>
+    <p className="mx-12 mt-4 text-gray-700">{category}</p>
+    <div className="flex flex-col md:flex-row p-4">
+      <img className="w-full px-4 basis-7/12" src={image} alt={title}/>
+      <div className="w-full basis-5/12 flex-col p-4">
+        <h2 className="text-3xl font-bold py-2">{title}</h2>
+        <p className="text-2xl font-bold py-2 border-b border-gray-400">{`${price}원`}</p>
+        <p className="py-2 text-lg">{description}</p>
+        <div className="flex items-center w-full">
+          <label htmlFor="option" className="text-brand font-bold">옵션 : </label>
+          <select id="option" className="p-2 m-4 border-2 border-dashed border-brand outline-none" onChange={handleChange} value={selectedOption}>
+            {options.map((option, index) => <option key={index}>{option}</option>)}
+          </select>
+        </div>
+        <Button text='장바구니에 추가' onClick={handleclick} />
+      </div>
+
+    </div>
+   </section>
   )
 }

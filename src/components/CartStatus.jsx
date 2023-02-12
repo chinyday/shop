c4cfcb81192fec77db9e04ce5cd1daa04e7cfca9
chinyday <chinyday@gmail.com>
2023-02-12 22:57:56 +0900

    feat: 장바구니 컴포넌트 생성 및 스타일링

---
+++ src/components/CartStatus.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { BiCartAlt } from 'react-icons/bi'
+import { getCart } from '../api/firebase';
+import { useAuthContext } from '../context/AuthContext';
+
+
+const CartStatus = () => {
+  const { uid } = useAuthContext();
+  const {data: products} = useQuery(['cart'], () => getCart(uid));
+  return (
+    <div className='relative'>
+      <BiCartAlt className='text-4xl' />
+      {products && <p className='absolute w-5 h-5 text-center bg-brand text-white rounded-full -top-1 -right-1 text-xs justify-center flex items-center'>{products.length}</p>}
+    </div>
+  );
+}
+
+export default CartStatus;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BiAddToQueue, BiCartAlt } from 'react-icons/bi'
+import { BiAddToQueue } from 'react-icons/bi'
 // import {login, logout, onUserStateChange} from  '../api/firebase'
 import User from "./User";
 import Button from "./ui/Button";
 import { useAuthContext } from "../context/AuthContext";
+import CartStatus from "./CartStatus";
 
 export default function NavBar() {
 
@@ -17,7 +18,7 @@ export default function NavBar() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to='/products'>products</Link>
-        {user && <Link to='/cart' className="text-2xl"><BiCartAlt /></Link>}
+        {user && <Link to='/cart' className="text-2xl"><CartStatus /></Link>}
         {user && user.isAdmin && <Link to='/products/new' className="text-2xl"><BiAddToQueue /></Link>}
         {user && <User user={user} />}
         {user ? <Button onClick={logout} text={'Logout'} /> : <Button onClick={login} text={'Login'} />}
